98724426b767e022c49c8af19f42a1028ed6957f
bebo <70724498+bebokurd@users.noreply.github.com>
2022-04-17 21:11:59 -0700

    Update chyatv.js

--- chyatv.js
+++ chyatv.js
@@ -15,7 +15,7 @@ function loophalaman(a) {
             2 == nomerhal
                 ? "page" == jenis
                     ? '<span class="showpage"><a href="' + home_page + '">' + upPageWord + "</a></span>"
-                    : '<span class="showpageNum"><a href="#/page/' + lblname1 + "?&max-results=" + postperpage + '">' + upPageWord + "</a></span>"
+                    : '<span class="showpageNum"><a href="/#/page/' + lblname1 + "?&max-results=" + postperpage + '">' + upPageWord + "</a></span>"
                 : "page" == jenis
                 ? '<span class="showpageNum"><a href="#" onclick="redirectpage(' + s + ');return false">' + upPageWord + "</a></span>"
                 : '<span class="showpageNum"><a href="#" onclick="redirectlabel(' + s + ');return false">' + upPageWord + "</a></span>"),
@@ -54,10 +54,10 @@ function hitungtotaldata(a) {
 }
 function halamanblogger() {
     var a = urlactivepage;
-    -1 != a.indexOf("/search/label/") && (lblname1 = -1 != a.indexOf("?updated-max") ? a.substring(a.indexOf("#/page/") + 14, a.indexOf("?updated-max")) : a.substring(a.indexOf("#/page/") + 14, a.indexOf("?&max"))),
+    -1 != a.indexOf("/search/label/") && (lblname1 = -1 != a.indexOf("?updated-max") ? a.substring(a.indexOf("/search/label/") + 14, a.indexOf("?updated-max")) : a.substring(a.indexOf("/search/label/") + 14, a.indexOf("?&max"))),
         -1 == a.indexOf("?q=") &&
             -1 == a.indexOf(".html") &&
-            (-1 == a.indexOf("#/page/")
+            (-1 == a.indexOf("/search/label/")
                 ? ((jenis = "page"),
                   (nomerhal = -1 != urlactivepage.indexOf("#PageNo=") ? urlactivepage.substring(urlactivepage.indexOf("#PageNo=") + 8, urlactivepage.length) : 1),
                   document.write('<script src="' + home_page + 'feeds/posts/summary?max-results=1&alt=json-in-script&callback=hitungtotaldata"></script>'))
@@ -83,7 +83,7 @@ function finddatepost(a) {
     var e = post.published.$t.substring(0, 19) + post.published.$t.substring(23, 29),
         s = encodeURIComponent(e);
     if ("page" == jenis) var r = "/search?updated-max=" + s + "&max-results=" + postperpage + "#PageNo=" + nopage;
-    else var r = "#/page/" + lblname1 + "?updated-max=" + s + "&max-results=" + postperpage + "#PageNo=" + nopage;
+    else var r = "/search/label/" + lblname1 + "?updated-max=" + s + "&max-results=" + postperpage + "#PageNo=" + nopage;
     location.href = r;
 }
 var nopage, jenis, nomerhal, lblname1;
